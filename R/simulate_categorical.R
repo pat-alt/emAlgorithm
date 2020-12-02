1acbf07db0a4d86a29baee3db0b7bf66473aec3c
pat-alt <altmeyerpat@gmail.com>
2020-12-02 23:26:56 +0100

    first baby steps

--- R/simulate_categorical.R
+++ R/simulate_categorical.R
@@ -1,21 +1,3 @@
-sim_categorical_fl <- function(n, prob) {
-  k <- length(prob)
-  # Normalise prob if necessary:
-  if (sum(prob)!=1) {
-    prob <- prob/sum(prob)
-  }
-  emp_cdf <- cumsum(prob)
-  # Matrix for 1-hot-encoding:
-  X <- matrix(rep(0, n*k), nrow = k, ncol = n)
-  U <- runif(n)
-  for (j in 1:n) {
-    i <- binary_search_rightmost(x=emp_cdf, target=U[j])
-    X[i,j] <- 1
-  }
-  return(X)
-}
-
-
 sim_categorical <- function(n, prob) {
   k <- length(prob)
   # Normalise prob if necessary:
