03696dfceb39981ab621c04cb430bee1d259b9f5
pat-alt <altmeyerpat@gmail.com>
2020-12-05 19:15:45 +0100

    just set up EM

--- R/em.R
+++ R/em.R
@@ -1,7 +1,25 @@
 em.multilevel_model <- function(multilevel_model, theta0=NULL) {
-  # Initial guess:
+  # Initialization: ----
   if (is.null(theta0)) {
     beta0 <- qr.solve(X, y)[,1] # initialize as pooled OLS
     gamma0 <- c(0,1) # initialize as u
   }
+  converged <- FALSE
+  # Recursion: ----
+  while (!converged) {
+    # 1.) E-step: ----
+
+    # 2.) M-step: ----
+  }
+  output <- list()
+  class(output) <- "em_output"
+  return(output)
+}
+
+em <- function(multilevel_model, theta0=NULL) {
+  UseMethod("em")
+}
+
+print.em_output <- function(output) {
+  cat(sprintf("Coefficients are: %0.2f", 0.01))
 }
